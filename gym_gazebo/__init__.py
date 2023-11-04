2926ffdd3be9958d81c44faef3db09e0b2bb0656
ChayceJRoss <chayceross108@gmail.com>
2023-11-03 20:25:55 -0700

    finished lab 8

--- gym_gazebo/__init__.py
+++ gym_gazebo/__init__.py
@@ -5,10 +5,17 @@ logger = logging.getLogger(__name__)
 
 # Gazebo
 # ----------------------------------------
-
-# cart pole
 register(
 	id='Gazebo_linefollow-v0',
 	entry_point='gym_gazebo.envs.gazebo_linefollow:Gazebo_Linefollow_Env',
 	max_episode_steps=3000,
 )
+
+
+# cart pole
+register(
+	id='GazeboCartPole-v0',
+	entry_point='gym_gazebo.envs.gazebo_cartpole:GazeboCartPolev0Env',
+	max_episode_steps=3000,
+)
+
